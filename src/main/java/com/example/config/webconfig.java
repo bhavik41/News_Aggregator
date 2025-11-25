3825cdf5916af21ce0d90a85ee47dd6a907f6d58
pateldhriti <pdhriti489@gmail.com>
2025-11-25 17:52:41 -0500

    dhruvi the god

--- src/main/java/com/example/config/webconfig.java
+++ src/main/java/com/example/config/webconfig.java
@@ -15,5 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+                // dhruvi the god
     }
 }
